132cc0b7ed4559da5e6d7602803310946aca61e0
Chris Nielsen <chris.rahbek.nielsen@gmail.com>
2026-02-19 21:24:55 +0100

    Adjusted configs to allow for imports for .vue files with no explicit export.

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,3 +1,10 @@
 /// <reference types="vite/client" />
 /// <reference types="unplugin-vue-router/client" />
 /// <reference types="vite-plugin-vue-layouts-next/client" />
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  // The {} represents the props, the second {} represents the raw data, and 'any' is the rest.
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
